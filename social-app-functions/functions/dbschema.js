63b5dbdb1c3a95684a2dc83b7a118e114f684dc2
vivekgondalia <vgondalia@drintl.com>
2020-01-02 16:14:47 -0500

    express and formatting response

---
+++ social-app-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    scerams: [
+        {
+            userHandle: 'user',
+            body: 'this is a scream body',
+            createdAt: "2020-01-02T21:09:31.092Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- social-app-functions/functions/index.js
+++ social-app-functions/functions/index.js
@@ -3,35 +3,35 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase World!");
-});
+const express = require('express');
+const app = express();
 
-
-exports.getScreams = functions.https.onRequest((request, response) => {
-    admin.firestore().collection('screams').get()
+app.get('/screams', (request, response) => {
+    admin.firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
         .then(data => {
             let screams = [];
             data.forEach(doc => {
-                screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
             });
             return response.json(screams);
         })
         .catch(err => console.error(err));
 })
 
-exports.createScreams  = functions.https.onRequest((request, response) => {
-    if(request.method !== "POST"){
-        return response.status(400).json({ error: "method not allowed"})
-    }
-    
+
+app.post('/scream', (request, response) => {
     const newScream = {
         body: request.body.body,
         userHandle: request.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin
@@ -46,3 +46,7 @@ exports.createScreams  = functions.https.onRequest((request, response) => {
             console.error(err);
         });
 })
+
+//best practice: https://baseurl.com/api/
+
+exports.api = functions.https.onRequest(app);
