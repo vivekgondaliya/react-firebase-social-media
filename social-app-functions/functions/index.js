8d05daa3737bab1c5aa9cc9755153955fe275f3a
vivekgondalia <vgondalia@drintl.com>
2020-01-02 18:33:21 -0500

    authentication middleware

--- social-app-functions/functions/index.js
+++ social-app-functions/functions/index.js
@@ -40,17 +40,48 @@ app.get('/screams', (request, response) => {
         .catch(err => console.error(err));
 })
 
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error("No token found")
+        return res.status(403).json({ error : 'Unauthorized'})
+    }
 
-app.post('/scream', (request, response) => {
+    admin.auth().verifyIdToken(idToken)
+        .then( decodedToken => {
+            req.user = decodedToken;
+            return db.collection('users')
+                .where('userId', "==", req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error("Error while verifying token", err);
+            return res.status(403).json(err);
+        })
+}
+
+//post one scream
+app.post('/scream', FBAuth, (request, response) => {
+
+    if(request.body.body.trim() === ''){
+        return res.status(400).json({ body: 'Body must not be empty'});
+    }
+
+
     const newScream = {
         body: request.body.body,
-        userHandle: request.body.userHandle,
+        userHandle: request.user.handle,
         createdAt: new Date().toISOString()
     };
 
-    admin
-        .firestore()
-        .collection('screams')
+    db.collection('screams')
         .add(newScream)
         .then((doc) => {
             response.json({message: `document ${doc.id} created successfully`});
