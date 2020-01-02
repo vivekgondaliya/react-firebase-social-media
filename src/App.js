1a4d3b5271c4b4425a56e1a7f7f4efe874e796f0
vivekgondalia <vgondalia@drintl.com>
2020-01-02 15:45:01 -0500

    init firebase, create & read data from firestore database

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import TopNavigation from './components/TopNavigation';
-import ContentContainer from "./components/ContentContainer";
-import About from './components/About';
-import NotFound from './components/NotFound';
-import Footer from './components/Footer';
-import WeatherDetailedInfo from './components/WeatherDetailedInfo';
-import WeatherTypes from './components/WeatherType';
-import LoginForm from './components/Login';
-
 class App extends Component {
     render() {
         return (
             <div>
-                <TopNavigation />
-                <Switch>
-                    <Route exact path="/" component={LoginForm} />
-                    <PrivateRoute path="/home">
-                        <ContentContainer />
-                    </PrivateRoute>
-                    <PrivateRoute path="/weather">
-                        <WeatherTypes />
-                    </PrivateRoute>
-                    <PrivateRoute path="/about">
-                        <About />
-                    </PrivateRoute>
-                    <Route component={NotFound} />
-                </Switch>
-                <Footer />
+                Hello World
             </div>
         )
     }
