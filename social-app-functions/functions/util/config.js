f7b60486cf06568659b64462c7d473de6a2b8e9f
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2020-01-02 22:18:34 -0500

    code refactor and reorganization

---
+++ social-app-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAZpeqoEsIs0ZjMZRMVhl9Okf_uwVZole0",
+    authDomain: "social-app-4d32e.firebaseapp.com",
+    databaseURL: "https://social-app-4d32e.firebaseio.com",
+    projectId: "social-app-4d32e",
+    storageBucket: "social-app-4d32e.appspot.com",
+    messagingSenderId: "488521372555",
+    appId: "1:488521372555:web:c177a9e5c27a7c10b2958e"
+};
